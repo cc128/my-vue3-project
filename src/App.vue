2ee07e4a61162d064d150efd834a66db61863fa1
lsc <1286439347@qq.com>
2022-04-25 00:11:08 +0800

    聊天室

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,82 @@
 <template>
     <router-view></router-view>
 </template>
+<script>
+import { getCurrentInstance, reactive, onMounted, computed, onBeforeUnmount } from 'vue'
+import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js";
+export default {
+    props: {
+        title: {
+            type: [Number, String],
+            deflert: ""
+        }
+    },
+    setup(props, { emit, slots, attrs }) {
+        let socket = io.connect("http://192.168.1.3:7005");
+        let socketId;
+        let nserName;
+        let userKey;
+        let userList;
+        //获取自己的id
+        socket.on("socketId", data => {
+            socketId = data.socketId;
+            localStorage.setItem("socketId", socketId);
+            if (!localStorage.getItem("nserName")) {
+                localStorage.setItem("nserName", data.socketId);
+            }
+            nserName = localStorage.getItem("nserName");
+
+            if (!localStorage.getItem("userKey")) {
+                localStorage.setItem("userKey", socketId);
+            }
+            userKey = localStorage.getItem("userKey");
+            sendLink(socketId); // 发送连接
+        });
+        let getUserList = () => {
+            socket.on("userList", data => {
+                this.userList = userList;
+            })
+        }
+
+        // 修改名字
+        let setName = () => {
+            let name = prompt("请输入你的名字", "王老五");
+            if (name != null && name != "") {
+                localStorage.setItem("name", name);
+                sendLink(); // 发送连接
+            } else {
+            }
+        }
+        // 发送连接
+        let sendLink = () => {
+            socket.emit("linkUser", {
+                socketId: socketId,
+                name: nserName,
+            })
+        }
+
+        const state = reactive({
+            three: null,
+            activeIndex2: "chatRoom",
+            // lowerCaseUsername: computed(() => state.username.toLowerCase()), //计算属性
+        })
+        const { ctx } = getCurrentInstance();
+        let handleSelect = (key, keyPath) => {
+            // console.log(key, keyPath)
+        }
+        onMounted(() => {
+            console.log(ctx)
+        });
+        // 组件销毁前
+        onBeforeUnmount(() => {
+        })
+        let fn = { handleSelect }
+        return { state, ...fn }
+    },
+    watch: {},
+    components: {},
+};
+</script>
 
 <style lang="scss">
 @import url("./styles/public.module.css");
--- src/views/chatRoom/index.vue
+++ src/views/chatRoom/index.vue
@@ -1,6 +1,31 @@
 <template>
     <div class="chat-room">
-        聊天室
+        <!-- <ul>
+            <li class="photo">
+                <img src="" alt="" srcset="">
+            </li>
+            <li class="content">内容</li>
+        </ul> -->
+        <el-timeline>
+            <el-timeline-item timestamp="2018/4/12" placement="top">
+                <el-card>
+                    <h4>{{ }}</h4>
+                    <p>Tom committed 2018/4/12 20:46</p>
+                </el-card>
+            </el-timeline-item>
+            <el-timeline-item timestamp="2018/4/3" placement="top">
+                <el-card>
+                    <h4>Update Github template</h4>
+                    <p>Tom committed 2018/4/3 20:46</p>
+                </el-card>
+            </el-timeline-item>
+            <el-timeline-item timestamp="2018/4/2" placement="top">
+                <el-card>
+                    <h4>Update Github template</h4>
+                    <p>Tom committed 2018/4/2 20:46</p>
+                </el-card>
+            </el-timeline-item>
+        </el-timeline>
     </div>
 </template>
 
@@ -40,5 +65,22 @@ export default {
 <style lang="scss">
 .chat-room {
     padding: 20px;
+    // ul {
+    //     display: flex;
+    //     align-items: center;
+    //     .photo {
+    //         width: 50px;
+    //         height: 50px;
+    //         border-radius: 50%;
+    //         background: #ccc;
+    //         margin-right: 10px;
+    //     }
+    //     .content {
+    //         border-radius: 5px;
+    //         background: #ddd;
+    //         padding: 10px;
+    //         flex: 1;
+    //     }
+    // }
 }
 </style>
